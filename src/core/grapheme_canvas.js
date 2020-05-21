0aff6ef9c62fe1038bd93325e118a90a440f7034
Timothy Herchen <timothy.herchen@gmail.com>
2020-05-21 12:28:22 -0700

    improve code quality pt. 1

--- src/core/grapheme_canvas.js
+++ src/core/grapheme_canvas.js
@@ -3,51 +3,47 @@ import { Element as GraphemeElement } from './grapheme_element'
 import * as utils from './utils'
 import { LabelManager } from './label_manager'
 
-/** A grapheme window is an actual viewable instance of Grapheme.
-That is, it is a div that can be put into the DOM and manipulated (and seen).
-
-Properties:
-domElement = the div that the user adds to the webpage
-textCanvas = the canvas that text and stuff is done on
-context = the parent Grapheme.Context of this window
-textCanvasContext = the Canvas2DRenderingContext associated with the textCanvas
-cssWidth, cssHeight = the size of the canvas in CSS pixels
-canvasWidth, canvasHeight = the actual size of the canvas in pixels
-*/
+/** @class GraphemeCanvas A viewable instance of Grapheme. Provides the information required for rendering to canvas. */
 class GraphemeCanvas extends GraphemeGroup {
+  /**
+   * Creates a GraphemeCanvas.
+   *
+   * @constructor
+   * @param params {Object} The parameters object.
+   */
   constructor (params={}) {
     super(params)
 
     // Element to be put into the webpage
-    this.domElement = document.createElement('div')
+    /** @public */ this.domElement = document.createElement('div')
 
-    // The canvas of a GraphemeWindow
-    this.canvas = document.createElement('canvas')
+    // The canvas of a GraphemeCanvas
+    /** @public */ this.canvas = document.createElement('canvas')
+
+    // Append the canvas to the dom element
     this.domElement.appendChild(this.canvas)
 
-    // CSS stuffs
+    // Enable CSS stuff
     this.canvas.classList.add('grapheme-canvas')
     this.domElement.classList.add('grapheme-window')
 
-    // Get the contexts
-    this.ctx = this.canvas.getContext('2d')
-    utils.assert(this.ctx, "This browser doesn't support 2D canvas, what the heck")
+    // CanvasRenderingContext2D for this GraphemeCanvas
+    /** @public */ this.ctx = this.canvas.getContext('2d')
 
-    // label manager
-    this.labelManager = new LabelManager(this.domElement)
+    // If no context, throw an error
+    if (!this.ctx)
+      throw new Error("This browser doesn't support 2D canvas, which is required for Grapheme. Please get a competent browser.")
+
+    // Label manager for LaTeX-enabled labels
+    /** @private */ this.labelManager = new LabelManager(this.domElement)
 
     // Set the default size to 640 by 480 in CSS pixels
     this.setSize(640, 480)
-
-    // Scale canvas as needed due to DPR
-    this.resetCanvasCtxTransform()
-
-    this.addEventListener('dprchanged', () => {
-      this.update();
-      return true;
-    })
   }
 
+  /**
+   * Resets the context's transform to scale up by the device pixel ratio
+   */
   resetCanvasCtxTransform () {
     const ctx = this.ctx
 
@@ -55,113 +51,121 @@ class GraphemeCanvas extends GraphemeGroup {
     ctx.scale(utils.dpr, utils.dpr)
   }
 
-  // Set the size of this window (including adjusting the canvas size)
-  // Note that this width and height are in CSS pixels
+  /**
+   * Set the size of this GraphemeCanvas. Note that width and height are in CSS pixels.
+   * @param width Desired width of canvas.
+   * @param height Desired height of canvas.
+   */
   setSize (width, height) {
-    // width and height are in CSS pixels
-    this.width = width
-    this.height = height
+    /** @public */ this.width = width
+    /** @public */ this.height = height
 
-    // Update the canvas size, factoring in the device pixel ratio
+    // Update the actual canvas's size, factoring in the device pixel ratio
     this.canvasWidth = this.width * utils.dpr
     this.canvasHeight = this.height * utils.dpr
 
-    // Set the canvas CSS size using CSS
+    // Set the canvas's display using CSS
     const canvas = this.canvas
 
     canvas.style.width = `${width}px`
     canvas.style.height = `${height}px`
 
+    // Scale up by device pixel ratio
+    this.resetCanvasCtxTransform()
+
+    // Trigger the resize event to let elements know to update
     this.triggerEvent("resize", {width, height})
   }
 
-  // Returns the pixel width of the canvas
+  /**
+   * Get the width of the canvas in displayed pixels (not CSS pixels).
+   * @returns {number} The width of the canvas.
+   */
   get canvasWidth () {
     return this.canvas.width
   }
 
-  // Returns the pixel height of the canvas
+  /**
+   * Get the height of the canvas in displayed pixels (not CSS pixels).
+   * @returns {number} The height of the canvas.
+   */
   get canvasHeight () {
     return this.canvas.height
   }
 
-  // Sets the pixel width of the canvas
-  set canvasWidth (x) {
+  /**
+   * Set the width of the canvas in displayed pixels
+   * @private
+   * @param width The desired width of the canvas.
+   */
+  set canvasWidth (width) {
     // Round it to an integer and make sure it's in a reasonable range
-    x = Math.round(x)
-    utils.assert(utils.isPositiveInteger(x) && x < 16384, 'canvas width must be in range [1,16383]')
+    width = Math.round(width)
+    utils.assert(utils.isPositiveInteger(width) && width < 16384, 'Canvas width must be in range [1,16383].')
 
-    this.canvas.width = x
+    this.canvas.width = width
   }
 
-  // Sets the pixel height of the canvas
-  set canvasHeight (y) {
-    y = Math.round(y)
-    utils.assert(utils.isPositiveInteger(y) && y < 16384, 'canvas height must be in range [1,16383]')
+  /**
+   * Set the height of the canvas in displayed pixels
+   * @private
+   * @param height The desired height of the canvas.
+   */
+  set canvasHeight (height) {
+    height = Math.round(height)
+    utils.assert(utils.isPositiveInteger(height) && height < 16384, 'Canvas height must be in range [1,16383].')
 
-    this.canvas.height = y
+    this.canvas.height = height
   }
 
-  // Event triggered when the device pixel ratio changes
-  onDPRChanged () {
-    // This will resize the canvas accordingly
-    this.setSize(this.width, this.height)
-
-    this.resetCanvasCtxTransform()
-  }
-
-  // Destroy this window.
+  /**
+   * Destroy this GraphemeCanvas
+   */
   destroy () {
-    // Destroy the domElement
+    // Destroy the DOM element
     this.domElement.remove()
 
     // Destroy the elements too, if desired
     super.destroy()
 
-    // Delete some references
+    // Delete some references to allow memory to be freed immediately
     delete this.canvas
-    delete this.domElement
     delete this.ctx
+    delete this.domElement
+    delete this.labelManager
   }
 
+  /**
+   * Clear the canvas
+   */
   clear () {
-    // Clear the canvas
     this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
   }
 
+  /**
+   * Render this GraphemeCanvas
+   */
   render () {
     const { labelManager, ctx } = this
+    const plot = this
 
-    // ID of this render
+    // Set ID of this render
     labelManager.currentRenderID = utils.getRenderID()
 
-    // Render information to be given to elements. Namely,
-    // labelManager
-    // ctx
-    // window
-    const info = {
-      labelManager,
-      ctx,
-      plot: this
-    }
+    // Info to be given to rendered elements
+    const info = { labelManager, ctx, plot }
 
+    // Clear the canvas
+    this.clear()
+
+    // Reset the rendering context transform
     this.resetCanvasCtxTransform()
 
-    let err // potential error in try {...} catch
-    try {
-      // Clear this canvas
-      this.clear()
+    // Render all children
+    super.render(info)
 
-      // Render all children
-      super.render(info)
-
-      // Get rid of old labels
-      labelManager.cleanOldRenders()
-    } catch (e) {
-      err = e
-    }
-
-    if (err) throw err
+    // Get rid of old labels
+    labelManager.cleanOldRenders()
   }
 }
 
