0150e3ab2a0eec95f72dbc6bcec2d41c457ee81d
Timothy Herchen <timothy.herchen@gmail.com>
2020-06-04 16:17:30 -0700

    finish (mostly) commenting core

--- build/grapheme.js
+++ build/grapheme.js
@@ -10067,23 +10067,38 @@ var Grapheme = (function (exports) {
     return points
   }
 
+  /**
+   * @class WebGLElement An element that supports WebGL rendering.
+   */
   class WebGLElement extends GraphemeElement {
+    /**
+     * Construct a new WebGLElement
+     * @param params Parameters
+     */
     constructor(params={}) {
       super(params);
 
-      this.id = generateUUID();
+      // id used for things like WebGL buffers
+      /** @protected */ this.id = generateUUID();
     }
 
+    /**
+     *
+     * @param info {Object} The render info
+     * @param info.beforeWebGLRender {Function} Prepare the universe for WebGL drawing
+     */
     render(info) {
-      if (info.beforeWebGLRender)
-        info.beforeWebGLRender();
+      // Call beforeWebGLRender()
+      info.beforeWebGLRender();
 
+      // Sort this element's children. We don't want to call super.render() because that will run beforeNormalRender
       this.sortChildren();
 
       // Update if needed
       if (this.alwaysUpdate)
         this.update();
 
+      // Render all children
       this.children.forEach(child => child.render(info));
     }
   }
--- src/core/webgl_grapheme_element.js
+++ src/core/webgl_grapheme_element.js
@@ -1,25 +1,40 @@
 import { Element as GraphemeElement } from './grapheme_element'
 import * as utils from "./utils"
 
+/**
+ * @class WebGLElement An element that supports WebGL rendering.
+ */
 class WebGLElement extends GraphemeElement {
+  /**
+   * Construct a new WebGLElement
+   * @param params Parameters
+   */
   constructor(params={}) {
     super(params)
 
-    this.id = utils.generateUUID()
+    // id used for things like WebGL buffers
+    /** @protected */ this.id = utils.generateUUID()
   }
 
+  /**
+   *
+   * @param info {Object} The render info
+   * @param info.beforeWebGLRender {Function} Prepare the universe for WebGL drawing
+   */
   render(info) {
-    if (info.beforeWebGLRender)
-      info.beforeWebGLRender()
+    // Call beforeWebGLRender()
+    info.beforeWebGLRender()
 
+    // Sort this element's children. We don't want to call super.render() because that will run beforeNormalRender
     this.sortChildren()
 
     // Update if needed
     if (this.alwaysUpdate)
       this.update()
 
+    // Render all children
     this.children.forEach(child => child.render(info))
   }
 }
 
-export {WebGLElement}
+export { WebGLElement }
