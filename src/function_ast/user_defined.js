ed5fb29c93ce812263b74df3fb25375e61e3913d
Timothy Herchen <timothy.herchen@gmail.com>
2020-06-27 03:41:30 -0700

    i've had enough for now

--- src/function_ast/user_defined.js
+++ src/function_ast/user_defined.js
@@ -2,12 +2,90 @@ import { ConstantNode, PI, E } from './node'
 import { OperatorList } from './operators'
 import * as utils from "../core/utils"
 
+const RESERVED_VARIABLE_NAMES = ['x', 'y', 'z', 't', 'i']
+const RESERVED_FUNCTION_NAMES = [...OperatorList]
+
 const Variables = {}
 const Functions = {}
 
+class DefinedBase {
+  constructor(node) {
+    this.node = node
+    this.wellDefined = false
+
+    this.dependencies = {}
+    this.compiled = {}
+  }
+
+  throwUndefined() {
+    if (!this.wellDefined)
+      throw new Error("The function/variable is not well defined. Issue: " + this.error)
+  }
+
+  getValue() {
+     this.throwUndefined()
+  }
+
+  getNormalFunc() {
+    this.throwUndefined()
+  }
+
+  getIntervalFunc() {
+    this.throwUndefined()
+
+  }
+
+  getRealFunc() {
+    this.throwUndefined()
+
+  }
+
+  getComplexFunc() {
+    this.throwUndefined()
+
+  }
+}
+
+class DefinedFunction {
+  constructor(name, node, exportedVariables) {
+    if (RESERVED_FUNCTION_NAMES.includes(name))
+      throw new Error("The name " + name + " is reserved. Please name your function something else.")
+
+    let dependencies = node.getDependencies()
+
+    for (let variable of exportedVariables.concat(RESERVED_VARIABLE_NAMES)) {
+      let index = dependencies.vars.indexOf(variable)
+
+      if (index !== -1) {
+        dependencies.vars.splice(index, 1)
+      }
+    }
+
+    for (let func of RESERVED_FUNCTION_NAMES) {
+      let index = dependencies.funcs.indexOf(variable)
+
+      if (index !== -1) {
+        dependencies.funcs.splice(index, 1)
+      }
+    }
+
+    this.dependencies = dependencies
+
+    this.checkWellDefined()
+  }
+
+
+
+  getValue() {
+    throw new Error("Value requested from function.")
+  }
+}
+
 class DefinedVariable {
-  constructor() {
-    
+  constructor(name) {
+    if (RESERVED_VARIABLE_NAMES.includes(name))
+      throw new Error("The name " + name + " is reserved. Please name your variable something else.")
+
   }
 }
 
