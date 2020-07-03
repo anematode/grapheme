2f3c72ce0c2409a3ffae030c3053e6f787611c50
Timothy Herchen <timothy.herchen@gmail.com>
2020-07-02 22:55:05 -0700

    stuff

--- src/function_ast/operator_definitions.js
+++ src/function_ast/operator_definitions.js
@@ -1,6 +1,30 @@
 
-const Operators = {
+class OperatorDefinition {
+  constructor() {
 
+  }
+}
+
+class NormalDefinition extends OperatorDefinition {
+  constructor() {
+    super()
+  }
+}
+
+class VariadicDefinition extends OperatorDefinition {
+  constructor() {
+    super()
+  }
+}
+
+const Operators = {
+  '*': [
+    new NormalDefinition({
+      signature: ["real", "real"],
+      returns: "real",
+      
+    })
+  ]
 }
 
 export
