47c21b6be04e76b8d0ca662a6933cd3bc81389f3
Timothy Herchen <timothy.herchen@gmail.com>
2021-09-16 10:22:35 -0700

    ijgro

--- src/elements/number_line/number_line_ticks.js
+++ src/elements/number_line/number_line_ticks.js
@@ -1,10 +1,24 @@
 import { constructInterface } from '../../core/interface.js'
+import { Element } from '../../core/element.js'
 
 const numberLineTicksInterface = constructInterface({
   interface: {
-    
+    ticks: { setAs: "user" }
   },
   internal: {
-
+    ticks: { computed: "none" }
   }
 })
+
+export class NumberLineTicks extends Element {
+  getInterface () {
+    return numberLineTicksInterface
+  }
+
+  _update () {
+    this.defaultInheritProps()
+    this.defaultComputeProps()
+
+
+  }
+}
